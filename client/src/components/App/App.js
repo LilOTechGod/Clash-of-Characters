e11401d5b6f69c842e343296c8847ccfc3288cd6
David Garcia <parradavid300@yahoo.com>
2022-12-07 11:53:17 -0600

    correcting a merge conflict with import css

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import styles from './styles.moduel.css'
+//import styles from './styles.module.css'
 import { StartMenu, Battle, Endmenu } from 'components';
 
 export const App = () => {
