8b78d03daf30075b4416041d24194a7727f577de
unknown <jimmymhuang@gmail.com>
2022-12-09 13:51:46 -0600

    implemeneted function to route start button on start menu to character select screen

--- client/src/components/StartMenu/StartMenu.js
+++ client/src/components/StartMenu/StartMenu.js
@@ -1,10 +1,12 @@
 import styles from './index.css';
 import BG from '../../assets/oak_woods_v1.0/background/background_layer_2.png';
 import '../StartMenu/index.css';
-
+import { useNavigate } from "react-router-dom";
 import React from 'react';
 
-export const StartMenu = ({ onStartClick }) => {
+export const StartMenu = () => {
+    const navigate = useNavigate();
+    const onStartClick = () => navigate('/characterSelect');
     return (
         <div className={styles.main}>
             <button className={styles.startButton} onClick={onStartClick} id='srtButton'>
