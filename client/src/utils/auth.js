5422bb8ec487d1c60a82d543bbf4ed167a3a5cf7
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-09 12:58:19 -0600

    fixed the login route

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -19,7 +19,7 @@ class AuthService {
       localStorage.removeItem('id_token');
       return true;
     }
-    // If token hasn't passed its expiration time, return `false`
+    
     return false;
   }
 
